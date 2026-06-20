6efecc01eb995bbb5e82ef0f7495520c5305e39c
Abah Prince Evans <evansabah2006@gmail.com>
2026-06-20 15:23:56 +0300

    fix(mobile): clean main.dart - remove hardcoded tokens

--- mobile_app_sxb_vpn/lib/main.dart
+++ mobile_app_sxb_vpn/lib/main.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'app/app.dart';
+  import 'package:flutter_riverpod/flutter_riverpod.dart';
+  import 'app/app.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.light,
-    ),
-  );
-
-  runApp(const ProviderScope(child: SxbVpnApp()));
-}
+  void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    runApp(
+      const ProviderScope(
+        child: SxBVpnApp(),
+      ),
+    );
+  }
+  
