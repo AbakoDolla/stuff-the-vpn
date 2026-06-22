b84f93cbb94de861c33b86141c03c805e12f3ad8
Abah Prince Evans <evansabah2006@gmail.com>
2026-06-22 09:43:50 +0300

    fix(android): remove () from flutter.versionCode and flutter.versionName in KTS

--- mobile_app_sxb_vpn/android/app/build.gradle.kts
+++ mobile_app_sxb_vpn/android/app/build.gradle.kts
@@ -1,40 +1,40 @@
 plugins {
-      id("com.android.application")
-      id("kotlin-android")
-      id("dev.flutter.flutter-gradle-plugin")
-  }
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
 
-  android {
-      namespace = "com.sxbvpn.app"
-      compileSdk = flutter.compileSdkVersion
+android {
+    namespace = "com.sxbvpn.app"
+    compileSdk = flutter.compileSdkVersion
 
-      compileOptions {
-          sourceCompatibility = JavaVersion.VERSION_11
-          targetCompatibility = JavaVersion.VERSION_11
-      }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
 
-      kotlinOptions {
-          jvmTarget = JavaVersion.VERSION_11.toString()
-      }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
 
-      defaultConfig {
-          applicationId = "com.sxbvpn.app"
-          minSdk = 21
-          targetSdk = flutter.targetSdkVersion
-          versionCode = flutter.versionCode()
-          versionName = flutter.versionName()
-      }
+    defaultConfig {
+        applicationId = "com.sxbvpn.app"
+        minSdk = 21
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
 
-      buildTypes {
-          release {
-              signingConfig = signingConfigs.getByName("debug")
-              isMinifyEnabled = false
-              isShrinkResources = false
-          }
-      }
-  }
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+}
 
-  flutter {
-      source = "../.."
-  }
-  
+flutter {
+    source = "../.."
+}
+
